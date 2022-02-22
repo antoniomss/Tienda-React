79ec62ad8ea58c1e7d0ef15ade6195e17f159fd3
Antonio <antonio_a.s@outlook.com>
2022-02-22 01:55:26 +0100

    tienda finalizada react

---
+++ src/Data2.js
@@ -0,0 +1,73 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Steamulation Pro X Mini",
+      price: 200,
+      image: "https://www.hispacachimba.es/image/cache/catalog/steamulation/steamulation-pro-x-mini-petrol-green-matt-1000x1500.jpg",
+      category: "cachimba",
+      description: "La mejor cachimba que te puedes llevar",
+      img1: "https://www.hispacachimba.es/image/cache/catalog/steamulation/steamulation-pro-x-mini-petrol-green-matt-1000x1500.jpg",
+      img2: "https://www.hispacachimba.es/image/cache/catalog/steamulation/steamulation-pro-x-mini-petrol-green-matt-1000x1500.jpg",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Moze Breeze Two",
+      price: 150,
+      image: "https://www.1001cachimbas.es/3700-thickbox_default/moze-breeze-two-wavy-red.jpg",
+      category: "cachimba",
+      description: "Una gran cachimba por ese precio",
+      img1: "https://www.1001cachimbas.es/3700-thickbox_default/moze-breeze-two-wavy-red.jpg",
+      img2: "https://www.1001cachimbas.es/3700-thickbox_default/moze-breeze-two-wavy-red.jpg",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Vyro x Moze Noir",
+      price: 179,
+      image: "https://www.el-badia.com/7075-large_default/chicha-vyro-x-moze-noir.jpg",
+      category: "cachimba",
+      description: "Cachimba muy bonita",
+      img1: "https://www.el-badia.com/7075-large_default/chicha-vyro-x-moze-noir.jpg",
+      img2: "https://www.el-badia.com/7075-large_default/chicha-vyro-x-moze-noir.jpg",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Cold Smoke Arrow Mini",
+      price: 79,
+      image: "https://www.1001cachimbas.es/3901-large_default/cold-smoke-arrow-mini-rose-gold.jpg",
+      category: "cachimba",
+      description: "Sencilla y barata. Podría usarse hasta de decoración",
+      img1: "https://www.1001cachimbas.es/3901-large_default/cold-smoke-arrow-mini-rose-gold.jpg",
+      img2: "https://www.1001cachimbas.es/3901-large_default/cold-smoke-arrow-mini-rose-gold.jpg",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "VZ Hookah Custom V2",
+      price: 350,
+      image: "https://www.hispacachimba.es/image/cache/catalog/rusia/cachimba-vz-hookah-custom-v2-1500x1342.jpg",
+      category: "cachimba",
+      description: "Es rusa.",
+      img1: " https://www.hispacachimba.es/image/cache/catalog/rusia/cachimba-vz-hookah-custom-v2-1500x1342.jpg",
+      img2: "https://www.hispacachimba.es/image/cache/catalog/rusia/cachimba-vz-hookah-custom-v2-1500x1342.jpg",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Aladin Epox 360 Pro",
+      price: 130,
+      image: "https://www.hispacachimba.es/image/cache/catalog/aladin/cachimba-aladin-epox-360-pro-red-blue-550x550.jpg",
+      category: "cachimba",
+      description: "Más purgas que Falete",
+      img1: "https://www.hispacachimba.es/image/cache/catalog/aladin/cachimba-aladin-epox-360-pro-red-blue-550x550.jpg",
+      img2: "https://www.hispacachimba.es/image/cache/catalog/aladin/cachimba-aladin-epox-360-pro-red-blue-550x550.jpg",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
